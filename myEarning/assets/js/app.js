4fb2616baeb4c3f3171ef9ab29cdea2d2af7ffcf
yinouet <ino25ctw@gmail.com>
2019-12-31 15:31:57 +0900

    フリーランス報酬計算機

---
+++ myEarning/assets/js/app.js
@@ -0,0 +1,37 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    expect: 1000,
+    weeklyOff: 2,
+    dailyWH: 8,
+    pto: 10,
+    nationalHolidays: 16,
+    errandsRate: 20
+  },
+  computed: {
+    annualOff: function() {
+      return this.weeklyOff * 52;
+    },
+    target: function() {
+      return this.expect * 1.3;
+    },
+    yearWD: function() {
+      return 365 - this.nationalHolidays - this.pto - this.annualOff;
+    },
+    actualRate: function() {
+      return (100 - this.errandsRate) / 100;
+    },
+    actualWD: function() {
+      return Math.round(this.yearWD * this.actualRate * 100) / 100;
+    },
+    actualWH: function() {
+      return Math.round(this.actualWD * this.dailyWH * 100) / 100;
+    },
+    resultDay: function() {
+      return Math.round((this.target * 10000) / this.actualWD);
+    },
+    resultHour: function() {
+      return Math.round((this.target * 10000) / this.actualWH);
+    }
+  }
+});
